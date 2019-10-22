60a06bb4e4c6a5bc4c3fc782fad0e3cac886f56e
Marcin Kostrzewski <Komar86311@gmail.com>
2019-10-22 23:45:47 +0200

    Z2_3 done

---
+++ Z2_3/UDP_30K_Client.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/time.h>
+
+#define BUFFER_SIZE 30000
+#define ATTEMPTS 100
+
+#define FILENAME "log.txt"
+
+char buf[BUFFER_SIZE];
+
+int main(int argc, char **argv)
+{
+    struct sockaddr_in endpoint;
+    int sdsocket, addrlen, received, i, r;
+    struct hostent *he;
+    struct timeval time_b, time_e;
+
+    if (argc<3) {
+        printf("Podaj nazwe hosta i numer portu jako parametry\n");
+        return 1;
+    }
+
+    he = gethostbyname(argv[1]);
+    if (he == NULL) {
+        printf("Nieznany host: %s\n",argv[1]);
+        return 0;
+    }
+
+    endpoint.sin_family = AF_INET;
+    endpoint.sin_port = htons(atoi(argv[2]));
+    endpoint.sin_addr = *(struct in_addr*) he->h_addr_list[0];
+    addrlen = sizeof(endpoint);   
+
+    gettimeofday(&time_b, NULL);
+
+    for (i=0; i<ATTEMPTS; i++){
+        if ((sdsocket = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+            printf("socket() %d nie powiodl sie\n", i);
+            return 1;
+        }
+        
+        r = sendto(sdsocket,
+            buf,
+            BUFFER_SIZE,
+            0,
+            (struct sockaddr*) &endpoint,
+            addrlen);
+        printf("Sent %d bytes\n", r);
+        received=0;
+        while (received < BUFFER_SIZE) {
+            received += recvfrom(sdsocket,
+                                buf+received,
+                                BUFFER_SIZE-received,
+                                0,
+                                (struct sockaddr*) &endpoint,
+                                &addrlen);
+
+            printf("Packets recieved. Bytes: %d\n", received);
+        }
+        close(sdsocket);
+    }   
+    gettimeofday(&time_e, NULL);
+
+    FILE *log=fopen(FILENAME, "a");
+    if(log==NULL) {
+        perror("Błąd pliku wyjściowego.");
+    } else {
+            fprintf(log, "Średnia wydajność protokołu UDP przy bloku 30 000 bajtów:\n");
+            fprintf(log, "czas: %.6f s\n",
+                                    (((double) (time_e.tv_sec - time_b.tv_sec) * 1000000) +
+                                    ((double) (time_e.tv_usec - time_b.tv_usec)))
+                                    / (1000000.0 * ATTEMPTS));
+    }
+    return 0;
+}
---
+++ Z2_3/UDP_30K_Server.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h> 
+
+#define BUFFER_SIZE 30000
+#define ATTEMPTS 100
+
+char buf[BUFFER_SIZE];
+
+int main(int argc, char **argv)
+{
+    struct sockaddr_in myaddr, endpoint;
+    int sdsocket, recieved, addrlen;
+
+    if (argc < 2) {
+        printf("podaj numer portu jako parametr\n");
+        return 1;
+    }
+
+    sdsocket = socket(AF_INET, SOCK_DGRAM, 0);
+    addrlen = sizeof(struct sockaddr_in);
+        
+    myaddr.sin_family = AF_INET;
+    myaddr.sin_port = htons(atoi(argv[1]));
+    myaddr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(sdsocket,(struct sockaddr*) &myaddr, addrlen) < 0) {
+        printf("bind() nie powiodl sie\n");
+        return 1;
+    }
+    
+    long long totalRecieved = 0;
+    while (totalRecieved < BUFFER_SIZE * ATTEMPTS) { /* nieskonczona petla */
+        recieved=0;
+        while (recieved < BUFFER_SIZE){
+            recieved += recvfrom(sdsocket,
+                                buf+recieved,
+                                BUFFER_SIZE-recieved,
+                                0,
+                                (struct sockaddr*) &endpoint,
+                                &addrlen);
+            totalRecieved += recieved;
+        }
+        printf("Recieved %d bytes\n", totalRecieved);
+        sendto(sdsocket, buf, BUFFER_SIZE, 0, (struct sockaddr*) &endpoint, addrlen);
+        printf("Sent packets\n");
+    }
+       
+    return 0;
+}
